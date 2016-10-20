b17dd500f66156dccb4d5fe40f7c615ab5ddb844
zliang <1090108015@qq.com>
2016-10-20 14:50:14 +0800

    添加Button按钮

--- app/src/main/java/com/example/testdemo/MainActivity.java
+++ app/src/main/java/com/example/testdemo/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.testdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv_test;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
